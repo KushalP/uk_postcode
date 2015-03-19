d2ae1be5125aa9a4221d0ac09f81f380a7ec247a
Kushal Pisavadia <kushi.p@gmail.com>
2015-03-19 17:23:36 +0000

    Add `valid?` fn to state if a postcode is valid
    A valid postcode has the following properties:
    
      - uppercase characters
      - has a valid outcode `@re_outcode_only`
      - has a valid outcode and incode `@re_full`
    
    The tests here cover these cases and make sure that an empty string
    isn't a valid postcode.

--- lib/uk_postcode.ex
+++ lib/uk_postcode.ex
@@ -1,2 +1,18 @@
 defmodule UKPostcode do
+  @re_outcode_only ~r/\A((?-mix:[A-Z]{1,2}[0-9R][0-9A-Z]?))\Z/
+  @re_full         ~r/\A((?-mix:[A-Z]{1,2}[0-9R][0-9A-Z]?)) ?((?-mix:[0-9][ABD-HJLNP-UW-Z]{2}))\Z/
+
+  def valid?(postcode) do
+    adjusted_postcode = postcode |>
+      String.strip |>
+      String.upcase
+
+    case adjusted_postcode do
+      "" ->
+        false
+      _ ->
+        Regex.match?(@re_outcode_only, adjusted_postcode) or
+        Regex.match?(@re_full, adjusted_postcode)
+    end
+  end
 end
--- test/uk_postcode_test.exs
+++ test/uk_postcode_test.exs
@@ -1,7 +1,36 @@
 defmodule UKPostcodeTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @valid_samples ["A9 9AA", "A99 9AA", "AA9 9AA", "AA99 9AA", "A9A 9AA", "AA9A 9AA",
+                  "SW1A 0AA", "SW1A 0PW", "SW1A 1AA", "SW1A 2HQ", "W1A 1AA", "W1A 1AB",
+                  "N81 1ER", "EH99 1SP"]
+  @valid_outcodes @valid_samples |>
+    Stream.map(&(String.split(&1, " "))) |>
+    Stream.map(&(hd(&1))) |>
+    Enum.to_list
+  @valid_incodes @valid_samples |>
+    Stream.map(&(String.split(&1, " "))) |>
+    Stream.map(&(tl(&1))) |>
+    Enum.to_list
+
+  test "empty strings aren't valid postcodes" do
+    Enum.map(["", " ", "\r\n", "\n"], &(refute UKPostcode.valid? &1))
+  end
+
+  test "full samples with spaces should all be valid" do
+    Enum.map(@valid_samples, &(assert UKPostcode.valid? &1))
+  end
+
+  test "full samples without spaces should all be valid" do
+    without_spaces = Enum.map(@valid_samples, &(String.replace(&1, " ", "")))
+    Enum.map(without_spaces, &(assert UKPostcode.valid? &1))
+  end
+
+  test "outcodes should all be valid" do
+    Enum.map(@valid_outcodes, &(assert UKPostcode.valid? &1))
+  end
+
+  test "lowercase postcode should be valid" do
+    assert UKPostcode.valid? "w1a 1aa"
   end
 end
