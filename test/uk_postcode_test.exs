42586fb690c134dc87c9fe80cacc18e8ba489b40
Kushal Pisavadia <kushi.p@gmail.com>
2015-03-19 21:50:43 +0000

    Switch to using ExSpec for tests
     - Switched `test` for `it`
     - Organised code into `describe` blocks

--- test/uk_postcode_test.exs
+++ test/uk_postcode_test.exs
@@ -1,5 +1,5 @@
 defmodule UKPostcodeTest do
-  use ExUnit.Case, async: true
+  use ExSpec, async: true
 
   @valid_samples ["A9 9AA", "A99 9AA", "AA9 9AA", "AA99 9AA", "A9A 9AA", "AA9A 9AA",
                   "SW1A 0AA", "SW1A 0PW", "SW1A 1AA", "SW1A 2HQ", "W1A 1AA", "W1A 1AB",
@@ -14,69 +14,81 @@ defmodule UKPostcodeTest do
     Enum.to_list |>
     List.flatten
 
-  test "empty strings aren't valid postcodes" do
-    Enum.map(["", " ", "\r\n", "\n"], &(refute UKPostcode.valid? &1))
+  describe "#valid?" do
+    it "shows empty strings as invalid postcodes" do
+      Enum.map(["", " ", "\r\n", "\n"], &(refute UKPostcode.valid? &1))
+    end
+
+    it "full samples with spaces should all be valid" do
+      Enum.map(@valid_samples, &(assert UKPostcode.valid? &1))
+    end
+
+    it "full samples without spaces should all be valid" do
+      without_spaces = Enum.map(@valid_samples, &(String.replace(&1, " ", "")))
+      Enum.map(without_spaces, &(assert UKPostcode.valid? &1))
+    end
+
+    it "outcodes should all be valid" do
+      Enum.map(@valid_outcodes, &(assert UKPostcode.valid? &1))
+    end
+
+    it "lowercase postcode should be valid" do
+      assert UKPostcode.valid? "w1a 1aa"
+    end
+
+    it "should not be valid if truncated" do
+      refute UKPostcode.valid? "W1A 1A"
+    end
   end
 
-  test "full samples with spaces should all be valid" do
-    Enum.map(@valid_samples, &(assert UKPostcode.valid? &1))
+  describe "#full?" do
+    it "full? only matches full postcode" do
+      Enum.map(@valid_samples, &(assert UKPostcode.full? &1))
+      Enum.map(@valid_outcodes, &(refute UKPostcode.full? &1))
+    end
   end
 
-  test "full samples without spaces should all be valid" do
-    without_spaces = Enum.map(@valid_samples, &(String.replace(&1, " ", "")))
-    Enum.map(without_spaces, &(assert UKPostcode.valid? &1))
+  describe "#outcode?" do
+    it "outcode? matches only outcodes of postcodes" do
+      Enum.map(@valid_samples, &(refute UKPostcode.outcode? &1))
+      Enum.map(@valid_outcodes, &(assert UKPostcode.outcode? &1))
+    end
   end
 
-  test "outcodes should all be valid" do
-    Enum.map(@valid_outcodes, &(assert UKPostcode.valid? &1))
+  describe "#outcode" do
+    it "can extract the outcode" do
+      Enum.map(Enum.zip(@valid_outcodes, @valid_samples),
+               &(assert {:ok, elem(&1, 0)} == UKPostcode.outcode(elem(&1, 1))))
+      assert {:ok, "A9"} == UKPostcode.outcode("A9")
+    end
+
+    it "outcode returns the provided value and errors when a valid postcode isn't provided" do
+      assert :error == elem(UKPostcode.outcode("FOO"), 0)
+      assert "FOO"  == elem(UKPostcode.outcode("FOO"), 1)
+    end
   end
 
-  test "lowercase postcode should be valid" do
-    assert UKPostcode.valid? "w1a 1aa"
+  describe "#incode" do
+    it "can extract the incode" do
+      Enum.map(Enum.zip(@valid_incodes, @valid_samples),
+               &(assert {:ok, elem(&1, 0)} == UKPostcode.incode(elem(&1, 1))))
+    end
+
+    it "incode returns the provided value and errors when a valid postcode isn't provided" do
+      assert :error == elem(UKPostcode.incode("FOO"), 0)
+      assert "FOO"  == elem(UKPostcode.incode("FOO"), 1)
+    end
   end
 
-  test "should not be valid if truncated" do
-    refute UKPostcode.valid? "W1A 1A"
-  end
+  describe "#normalise" do
+    it "can normalise a badly formatted postcode" do
+      assert "W1A 1AA" == UKPostcode.normalise "W1A1AA"
+      assert "W1A 1AA" == UKPostcode.normalise "w1a 1aa"
+    end
 
-  test "full? only matches full postcode" do
-    Enum.map(@valid_samples, &(assert UKPostcode.full? &1))
-    Enum.map(@valid_outcodes, &(refute UKPostcode.full? &1))
-  end
-
-  test "outcode? matches only outcodes of postcodes" do
-    Enum.map(@valid_samples, &(refute UKPostcode.outcode? &1))
-    Enum.map(@valid_outcodes, &(assert UKPostcode.outcode? &1))
-  end
-
-  test "can extract the outcode" do
-    Enum.map(Enum.zip(@valid_outcodes, @valid_samples),
-             &(assert {:ok, elem(&1, 0)} == UKPostcode.outcode(elem(&1, 1))))
-    assert {:ok, "A9"} == UKPostcode.outcode("A9")
-  end
-
-  test "outcode returns the provided value and errors when a valid postcode isn't provided" do
-    assert :error == elem(UKPostcode.outcode("FOO"), 0)
-    assert "FOO"  == elem(UKPostcode.outcode("FOO"), 1)
-  end
-
-  test "can extract the incode" do
-    Enum.map(Enum.zip(@valid_incodes, @valid_samples),
-             &(assert {:ok, elem(&1, 0)} == UKPostcode.incode(elem(&1, 1))))
-  end
-
-  test "incode returns the provided value and errors when a valid postcode isn't provided" do
-    assert :error == elem(UKPostcode.incode("FOO"), 0)
-    assert "FOO"  == elem(UKPostcode.incode("FOO"), 1)
-  end
-
-  test "can normalise a badly formatted postcode" do
-    assert "W1A 1AA" == UKPostcode.normalise "W1A1AA"
-    assert "W1A 1AA" == UKPostcode.normalise "w1a 1aa"
-  end
-
-  test "normalising using an invalid outcode or incode returns an error" do
-    assert :error == elem(UKPostcode.normalise("FOO"), 0)
-    assert "FOO"  == elem(UKPostcode.normalise("FOO"), 1)
+    it "normalising using an invalid outcode or incode returns an error" do
+      assert :error == elem(UKPostcode.normalise("FOO"), 0)
+      assert "FOO"  == elem(UKPostcode.normalise("FOO"), 1)
+    end
   end
 end
