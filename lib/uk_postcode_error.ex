59122fd5e56d9749f5627ffde800fdb6daf423f6
Kushal Pisavadia <kushi.p@gmail.com>
2018-06-19 17:17:49 +0100

    Provide docstrings for UKPostcode.Error

--- lib/uk_postcode_error.ex
+++ lib/uk_postcode_error.ex
@@ -1,6 +1,12 @@
 defmodule UKPostcode.Error do
+  @doc "Defines the UKPostcode exception format"
   defexception [reason: nil, value: nil]
 
+  @doc """
+  Converts a UKPostcode.Error into a String
+
+  exception - A UKPostcode.Error exception
+  """
   def message(exception) do
     "#{exception.value}: #{exception.reason}"
   end
